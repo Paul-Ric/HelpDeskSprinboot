0499a3f329626b72121d7f06e259e434a5d8b1ba
Paul-Ric <pauloricardo1060@gmail.com>
2022-05-26 00:27:45 -0300

    projeto iniciado

---
+++ src/test/java/com/pauloric/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pauloric.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
